92a0f3e63d9eb31412a35f25f06629d9f03ba5cb
Paul Feuvraux <paul@muonium.ee>
2017-07-23 20:25:23 +0200

    init
    Signed-off-by: Paul Feuvraux <paul@muonium.ee>

---
+++ README.md
@@ -0,0 +1,10 @@
+Script I made to install wire on my desktop.
+
+That's split in several functions, use the ones you want.
+
+# Usage
+
+```shell
+source install.sh
+function_you_want;
+```
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+deps(){
+  apt install git -y;
+}
+
+nodeJS_install(){
+  git clone https://github.com/nodejs/node
+  cd node;
+  ./configure
+  p=$(nproc)
+  make -j$p || exit 1;
+  make install || exit 2;
+  unset p;
+}
+
+wire_install(){
+  if [ ! -d /opt/ ];then
+    mkdir -p /opt/;
+  fi
+
+  cd /opt;
+  git clone https://github.com/wireapp/wire-desktop
+  cd wire-desktop;
+  npm install;
+  read -p "Run it?" nyan
+  if [ ! -z "$nyan" ];then
+    npm start&
+    unset nyan;
+  fi
+
+}
